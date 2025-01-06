2d76ca9c251150beb1bda87440c2cc87a29dbfc4
JoHyerin <gpfls468@gmail.com>
2025-01-06 22:19:18 +0900

    [Add] #3 - 프로젝트 생성 및 폴더링

---
+++ Napzakmarket-iOS/Napzakmarket-iOS/Application/Napzakmarket_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Napzakmarket_iOSApp.swift
+//  Napzakmarket-iOS
+//
+//  Created by 조혜린 on 1/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Napzakmarket_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Home/View/HomeView.swift
@@ -0,0 +1,7 @@
+//
+//  HomeView.swift
+//  Napzakmarket-iOS
+//
+//  Created by 조혜린 on 1/6/25.
+//
+
