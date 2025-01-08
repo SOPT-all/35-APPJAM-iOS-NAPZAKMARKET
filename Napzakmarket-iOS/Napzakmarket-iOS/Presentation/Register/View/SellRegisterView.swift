1a3e05940665d7a16a1e41ec7e9d9fb766c405ef
OneTen19 <khy000426@naver.com>
2025-01-09 02:44:47 +0900

    [Feat] - #5 등록하기 버튼

--- Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Register/View/SellRegisterView.swift
+++ Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Register/View/SellRegisterView.swift
@@ -40,191 +40,191 @@ struct SellRegisterView: View {
             ScrollView {
                 VStack(alignment: .leading) {
                     
-//                    // MARK: - 상품 이미지
-//                    
-//                    VStack(alignment: .leading, spacing: 4) {
-//                        Text("상품 이미지")
-//                            .font(.system(size: 16))
-//                        
-//                        Text("다양한 상품 이미지를 등록하면 판매 확률이 올라가요!")
-//                            .font(.system(size: 14))
-//                            .foregroundStyle(.gray)
-//                        
-//                        Button {
-//                            print("버튼 눌림")
-//                        } label: {
-//                            VStack(alignment: .center){
-//                                Image(systemName: "camera")
-//                                Text("0/10")
-//                            }
-//                        }
-//                        .frame(width: 80, height: 80)
-//                        .background(.gray)
-//                        .foregroundStyle(.white)
-//                        .clipShape(.rect(cornerRadius: 12))
-//                        .padding(.top, 12)
-//                    }
-//                    .padding(.vertical, 20)
-//                    .padding(.horizontal, 20)
-//                    
-//                    
-//                    // MARK: - 장르
-//                    
-//                    HStack(alignment: .center) {
-//                        NavigationLink {
-//                            Text("장르 뷰")
-//                        } label: {
-//                            HStack{
-//                                Text("장르")
-//                                    .foregroundStyle(.black)
-//                                    .font(.system(size: 16))
-//                                Spacer()
-//                                Image(systemName: "chevron.right")
-//                                    .foregroundStyle(.gray)
-//                            }
-//                        }
-//                        
-//                        Spacer()
-//                    }
-//                    .padding(.vertical, 20)
-//                    .padding(.horizontal, 20)
-//
-//                    
-//                    // MARK: - 구분선
-//                    
-//                    Divider()
-//                        .frame(height: 1)
-//                        .background(.gray)
-//                        .padding(.horizontal, 20)
-//                    
-//                    
-//                    // MARK: - 제목
-//                    
-//                    VStack(alignment: .leading) {
-//                        Text("제목")
-//                            .padding(.bottom, 12)
-//                        
-//                        TextField("제목을 적어주세요.", text: $title)
-//                            .maxLength(50, text: $title)
-//                            .padding(.horizontal, 14)
-//                            .padding(.vertical, 11)
-//                            .background {
-//                                RoundedRectangle(cornerRadius: 12)
-//                                    .stroke(.gray, lineWidth: 1)
-//                            }
-//                        
-//                        HStack {
-//                            Spacer()
-//                            Text("\(title.count) / 50")
-//                                .font(.system(size: 12))
-//                                .foregroundStyle(.gray)
-//                        }
-//                        .padding(.top, 4)
-//                        
-//                    }
-//                    .padding(.vertical, 35)
-//                    .padding(.horizontal, 20)
-//                    
-//                    
-//                    // MARK: - 설명
-//                    
-//                    VStack(alignment: .leading) {
-//                        Text("설명")
-//                            .padding(.bottom, 12)
-//                        
-//                        ZStack(alignment: .topLeading){
-//                            TextEditor(text: $description)
-//                                .maxLength(250, text: $description)
-//                                .padding(.horizontal, 14)
-//                                .padding(.vertical, 11)
-//                            
-//                            if description.isEmpty {
-//                                Text(descriptionPlaceholder)
-//                                    .foregroundStyle(.gray)
-//                                    .padding(.horizontal, 20)
-//                                    .padding(.vertical, 19)
-//                            }
-//                        }
-//                        .frame(height: 132)
-//                        .overlay {
-//                            RoundedRectangle(cornerRadius: 12)
-//                                .stroke(.gray, lineWidth: 1)
-//                        }
-//                        
-//                        HStack {
-//                            Spacer()
-//                            Text("\(description.count) / 250")
-//                                .font(.system(size: 12))
-//                                .foregroundStyle(.gray)
-//                        }
-//                        .padding(.top, 4)
-//                        
-//                    }
-//                    .padding(.bottom, 35)
-//                    .padding(.horizontal, 20)
-//
-//                    
-//                    // MARK: - 상품 상태
-//                    
-//                    VStack(alignment: .leading) {
-//                        Text("상품 상태")
-//                            .padding(.bottom, 12)
-//                        
-//                        LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 8) {
-//                            ForEach(options, id: \.self) { option in
-//                                Button {
-//                                    selectedOption = option
-//                                } label: {
-//                                    Text(option)
-//                                        .frame(maxWidth: .infinity, minHeight: 40)
-//                                        .foregroundColor(selectedOption == option ? .white : .black)
-//                                        .background(selectedOption == option ? .black : .white)
-//                                        .clipShape(.rect(cornerRadius: 12))
-//                                        .overlay(
-//                                            RoundedRectangle(cornerRadius: 12)
-//                                                .stroke(.gray, lineWidth: 1))
-//                                        
-//                                }
-//                            }
-//                        }
-//                        
-//                    }
-//                    .padding(.horizontal, 20)
-//
-//                    
-//                    // MARK: - 섹션 구분선
-//                    
-//                    Rectangle()
-//                        .fill(Color.gray.opacity(0.1))
-//                        .frame(height: 8)
-//                        .padding(.vertical, 40)
-//                    
-//                    
-//                    // MARK: - 가격
-//                    
-//                    VStack(alignment: .leading) {
-//                        Text("가격")
-//                            .padding(.bottom, 12)
-//                        
-//                        HStack{
-//                            TextField("0", text: $price)
-//                                .onChange(of: price) { oldValue, newValue in
-//                                    price = stringToPrice(input: newValue)
-//                                }
-//                                
-//                            Text("원")
-//                                .foregroundColor(.gray)
-//                        }
-//                        .padding(.horizontal, 14)
-//                        .padding(.vertical, 11)
-//                        .overlay {
-//                            RoundedRectangle(cornerRadius: 12)
-//                                .stroke(.gray, lineWidth: 1)
-//                        }
-//
-//                    }
-//                    .padding(.horizontal, 20)
+                    // MARK: - 상품 이미지
                     
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("상품 이미지")
+                            .font(.system(size: 16))
+                        
+                        Text("다양한 상품 이미지를 등록하면 판매 확률이 올라가요!")
+                            .font(.system(size: 14))
+                            .foregroundStyle(.gray)
+                        
+                        Button {
+                            print("버튼 눌림")
+                        } label: {
+                            VStack(alignment: .center){
+                                Image(systemName: "camera")
+                                Text("0/10")
+                            }
+                        }
+                        .frame(width: 80, height: 80)
+                        .background(.gray)
+                        .foregroundStyle(.white)
+                        .clipShape(.rect(cornerRadius: 12))
+                        .padding(.top, 12)
+                    }
+                    .padding(.vertical, 20)
+                    .padding(.horizontal, 20)
+                    
+                    
+                    // MARK: - 장르
+                    
+                    HStack(alignment: .center) {
+                        NavigationLink {
+                            Text("장르 뷰")
+                        } label: {
+                            HStack{
+                                Text("장르")
+                                    .foregroundStyle(.black)
+                                    .font(.system(size: 16))
+                                Spacer()
+                                Image(systemName: "chevron.right")
+                                    .foregroundStyle(.gray)
+                            }
+                        }
+                        
+                        Spacer()
+                    }
+                    .padding(.vertical, 20)
+                    .padding(.horizontal, 20)
+                    
+                    
+                    // MARK: - 구분선
+                    
+                    Divider()
+                        .frame(height: 1)
+                        .background(.gray)
+                        .padding(.horizontal, 20)
+                    
+                    
+                    // MARK: - 제목
+                    
+                    VStack(alignment: .leading) {
+                        Text("제목")
+                            .padding(.bottom, 12)
+                        
+                        TextField("제목을 적어주세요.", text: $title)
+                            .maxLength(50, text: $title)
+                            .padding(.horizontal, 14)
+                            .padding(.vertical, 11)
+                            .background {
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(.gray, lineWidth: 1)
+                            }
+                        
+                        HStack {
+                            Spacer()
+                            Text("\(title.count) / 50")
+                                .font(.system(size: 12))
+                                .foregroundStyle(.gray)
+                        }
+                        .padding(.top, 4)
+                        
+                    }
+                    .padding(.vertical, 35)
+                    .padding(.horizontal, 20)
+                    
+                    
+                    // MARK: - 설명
+                    
+                    VStack(alignment: .leading) {
+                        Text("설명")
+                            .padding(.bottom, 12)
+                        
+                        ZStack(alignment: .topLeading){
+                            TextEditor(text: $description)
+                                .maxLength(250, text: $description)
+                                .padding(.horizontal, 14)
+                                .padding(.vertical, 11)
+                            
+                            if description.isEmpty {
+                                Text(descriptionPlaceholder)
+                                    .foregroundStyle(.gray)
+                                    .padding(.horizontal, 20)
+                                    .padding(.vertical, 19)
+                            }
+                        }
+                        .frame(height: 132)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(.gray, lineWidth: 1)
+                        }
+                        
+                        HStack {
+                            Spacer()
+                            Text("\(description.count) / 250")
+                                .font(.system(size: 12))
+                                .foregroundStyle(.gray)
+                        }
+                        .padding(.top, 4)
+                        
+                    }
+                    .padding(.bottom, 35)
+                    .padding(.horizontal, 20)
+                    
+                    
+                    // MARK: - 상품 상태
+                    
+                    VStack(alignment: .leading) {
+                        Text("상품 상태")
+                            .padding(.bottom, 12)
+                        
+                        LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 8) {
+                            ForEach(options, id: \.self) { option in
+                                Button {
+                                    selectedOption = option
+                                } label: {
+                                    Text(option)
+                                        .frame(maxWidth: .infinity, minHeight: 40)
+                                        .foregroundColor(selectedOption == option ? .white : .black)
+                                        .background(selectedOption == option ? .black : .white)
+                                        .clipShape(.rect(cornerRadius: 12))
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 12)
+                                                .stroke(.gray, lineWidth: 1))
+                                    
+                                }
+                            }
+                        }
+                        
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    // MARK: - 섹션 구분선
+                    
+                    Rectangle()
+                        .fill(Color.gray.opacity(0.1))
+                        .frame(height: 8)
+                        .padding(.vertical, 40)
+                    
+                    
+                    // MARK: - 가격
+                    
+                    VStack(alignment: .leading) {
+                        Text("가격")
+                            .padding(.bottom, 12)
+                        
+                        HStack{
+                            TextField("0", text: $price)
+                                .onChange(of: price) { oldValue, newValue in
+                                    price = stringToPrice(input: newValue)
+                                }
+                            
+                            Text("원")
+                                .foregroundColor(.gray)
+                        }
+                        .padding(.horizontal, 14)
+                        .padding(.vertical, 11)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(.gray, lineWidth: 1)
+                        }
+                        
+                    }
+                    .padding(.bottom, 35)
+                    .padding(.horizontal, 20)
+
                     
                     // MARK: - 배송비
                     
@@ -252,7 +252,7 @@ struct SellRegisterView: View {
                                     .clipShape(.rect(cornerRadius: 12))
                                     .foregroundStyle(deliveryChargeFree ? .gray : .black)
                             }
-
+                            
                         }
                         .padding(3)
                         .background(.gray.opacity(0.2))
@@ -347,18 +347,29 @@ struct SellRegisterView: View {
                             }
                             
                         }
-
+                        
                     }
-                    
-                    
+                    .padding(.horizontal, 20)
+
                 }
-                .padding(.horizontal, 20)
-                .padding(.vertical, 35)
-                
-                
+                .padding(.bottom, 67)
             }
+            
+            Button(action: {
+                print(("tapped button"))
+            }, label: {
+                Text("등록하기")
+            })
+            .frame(maxWidth: .infinity, minHeight: 52)
+            .foregroundStyle(.white)
+            .background(.gray.opacity(1))
+            .clipShape(.rect(cornerRadius: 12))
+            .padding(.horizontal, 20)
+            .padding(.vertical, 20)
+            
         }
         .scrollIndicators(.hidden)
+        
         
     }
     
