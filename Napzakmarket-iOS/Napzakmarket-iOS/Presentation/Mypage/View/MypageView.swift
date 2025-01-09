ae46b8d522f17b9f607bf4bc9558309ffad65ed8
lalaurel <ajinpark0518@naver.com>
2025-01-10 05:04:21 +0900

    [Feat] - 마이페이지 임시 코드 작성

--- Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Mypage/View/MypageView.swift
+++ Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Mypage/View/MypageView.swift
@@ -5,3 +5,13 @@
 //  Created by 조혜린 on 1/6/25.
 //
 
+import SwiftUI
+
+struct MypageView: View {
+    var body: some View {
+        NavigationView {
+            Text("마이페이지")
+                .navigationTitle("마이")
+        }
+    }
+}
