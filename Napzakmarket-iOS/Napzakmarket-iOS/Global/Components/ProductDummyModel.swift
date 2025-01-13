2a6ae463f1c533b19ce962348d6f3ae23408f43a
JoHyerin <gpfls468@gmail.com>
2025-01-13 14:57:06 +0900

    [Feat] #15 - 상품 목록 스크롤 뷰 구현

--- Napzakmarket-iOS/Napzakmarket-iOS/Global/Components/ProductDummyModel.swift
+++ Napzakmarket-iOS/Napzakmarket-iOS/Global/Components/ProductDummyModel.swift
@@ -18,3 +18,85 @@ struct ProductDummyModel: Hashable {
     let productType: ProductType
     let isPriceNegotiable: Bool?
 }
+
+extension ProductDummyModel {
+    static let dummy = [
+        ProductDummyModel(
+            id: .init(),
+            isLikeButtonExist: true,
+            isLiked: false,
+            genreName: "산리오",
+            productName: "딸기 마이멜로디 마스코트 인형",
+            price: "35,000원",
+            uploadTime: "3시간 전",
+            productType: .buy,
+            isPriceNegotiable: false
+        ),
+        ProductDummyModel(
+            id: .init(),
+            isLikeButtonExist: true,
+            isLiked: false,
+            genreName: "산리오",
+            productName: "딸기 마이멜로디 마스코트 인형",
+            price: "35,000원",
+            uploadTime: "3시간 전",
+            productType: .buy,
+            isPriceNegotiable: false
+        ),
+        ProductDummyModel(
+            id: .init(),
+            isLikeButtonExist: true,
+            isLiked: false,
+            genreName: "산리오",
+            productName: "딸기 마이멜로디 마스코트 인형",
+            price: "35,000원",
+            uploadTime: "3시간 전",
+            productType: .buy,
+            isPriceNegotiable: false
+        ),
+        ProductDummyModel(
+            id: .init(),
+            isLikeButtonExist: true,
+            isLiked: false,
+            genreName: "산리오",
+            productName: "딸기 마이멜로디 마스코트 인형",
+            price: "35,000원",
+            uploadTime: "3시간 전",
+            productType: .buy,
+            isPriceNegotiable: false
+        ),
+        ProductDummyModel(
+            id: .init(),
+            isLikeButtonExist: true,
+            isLiked: false,
+            genreName: "산리오",
+            productName: "딸기 마이멜로디 마스코트 인형",
+            price: "35,000원",
+            uploadTime: "3시간 전",
+            productType: .buy,
+            isPriceNegotiable: false
+        ),
+        ProductDummyModel(
+            id: .init(),
+            isLikeButtonExist: true,
+            isLiked: false,
+            genreName: "산리오",
+            productName: "딸기 마이멜로디 마스코트 인형",
+            price: "35,000원",
+            uploadTime: "3시간 전",
+            productType: .buy,
+            isPriceNegotiable: false
+        ),
+        ProductDummyModel(
+            id: .init(),
+            isLikeButtonExist: true,
+            isLiked: false,
+            genreName: "산리오",
+            productName: "딸기 마이멜로디 마스코트 인형",
+            price: "35,000원",
+            uploadTime: "3시간 전",
+            productType: .buy,
+            isPriceNegotiable: false
+        )
+    ]
+}
--- Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Search/View/SearchView.swift
+++ Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Search/View/SearchView.swift
@@ -5,3 +5,36 @@
 //  Created by 조혜린 on 1/6/25.
 //
 
+import SwiftUI
+
+struct SearchView: View {
+    
+    //MARK: - Properties
+
+    let products = ProductDummyModel.dummy
+
+    let columns = [GridItem(.flexible(), spacing: 15), GridItem(.flexible(), spacing: 15)]    
+    
+    //MARK: - Main Body
+    var body: some View {
+        VStack {
+            productScrollView
+        }
+    }
+}
+
+extension SearchView {
+    
+    //MARK: - Properties
+    
+    private var productScrollView: some View {
+        ScrollView(showsIndicators: false) {
+            LazyVGrid(columns: columns, spacing: 20) {
+                ForEach(products, id: \.self) { product in
+                    ProductItemView(product: product)
+                }
+            }
+        }
+        .padding(.horizontal, 20)
+    }
+}
