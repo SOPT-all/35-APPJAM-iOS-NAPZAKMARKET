f6de5f8e21d2963931a8c4def86521a5ec4b2ce3
joho2022 <josama1997@naver.com>
2025-01-14 23:12:45 +0900

    [Feat] #9 - 장르 GridView 구현

---
+++ Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Onboarding/View/Genre/GenreCell.swift
@@ -0,0 +1,87 @@
+//
+//  GenreCell.swift
+//  Napzakmarket-iOS
+//
+//  Created by 조호근 on 1/13/25.
+//
+
+import SwiftUI
+import Kingfisher
+
+struct GenreCell: View {
+    
+    let genre: Genre
+    @Binding var isSelected: Bool
+    
+    
+    var body: some View {
+        VStack(spacing: 6) {
+            GenreImageView(imageUrl: genre.image, isSelected: isSelected)
+            GenreTextView(name: genre.name, isSelected: isSelected)
+        }
+        .onTapGesture {
+            isSelected.toggle()
+        }
+        .animation(.easeInOut(duration: 0.3), value: isSelected)
+        .frame(width: 100, height: 128)
+    }
+    
+}
+
+extension GenreCell {
+    
+    private struct GenreImageView: View {
+        let imageUrl: String?
+        let isSelected: Bool
+        
+        var body: some View {
+            ZStack(alignment: .topTrailing) {
+                if let imageURL = imageUrl,
+                   let url = URL(string: imageURL) {
+                    KFImage(url)
+                        .placeholder {
+                            Image(.imgOnboardingEmpty)
+                        }.retry(maxCount: 3, interval: .seconds(5))
+                        .onFailure { error  in
+                            print("failure: \(error.localizedDescription)")
+                        }
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100)
+                        .cornerRadius(12)
+                    
+                } else {
+                    Color.napzakGrayScale(.gray300)
+                        .frame(width: 100, height: 100)
+                        .cornerRadius(12)
+                }
+                
+                if isSelected {
+                    Image(systemName: "checkmark.square.fill")
+                        .resizable()
+                        .foregroundStyle(Color.napzakPuple(.purple30))
+                        .frame(width: 16, height: 16)
+                        .transition(.scale.combined(with: .opacity))
+                        .padding([.top, .trailing], 10)
+                }
+            }
+        }
+        
+    }
+    
+    private struct GenreTextView: View {
+        let name: String
+        let isSelected: Bool
+        
+        var body: some View {
+            Text(name)
+                .font(.napzakFont(.body2SemiBold16))
+                .applyNapzakTextStyle(napzakFontStyle: .body2SemiBold16)
+                .foregroundColor(
+                    isSelected ? .napzakPuple(.purple30) : .napzakGrayScale(.gray900)
+                )
+                .lineLimit(1)
+        }
+    }
+    
+}
---
+++ Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Onboarding/View/Genre/GenreGridView.swift
@@ -0,0 +1,49 @@
+//
+//  GenreGridView.swift
+//  Napzakmarket-iOS
+//
+//  Created by 조호근 on 1/14/25.
+//
+
+import SwiftUI
+
+struct GenreGridView: View {
+    
+    @Binding var genres: [Genre]
+    @Binding var selectedGenres: [Genre]
+    
+    private let colums = [
+        GridItem(.flexible()),
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: colums, spacing: 20) {
+                ForEach(genres) { genre in
+                    GenreCell(
+                        genre: genre,
+                        isSelected: Binding(
+                            get: { selectedGenres.contains(genre) },
+                            set: { isSelected in
+                                if isSelected {
+                                    if selectedGenres.count < 4 {
+                                        selectedGenres.append(genre)
+                                    }
+                                } else {
+                                    selectedGenres.removeAll(where: { $0.id == genre.id })
+                                }
+                            }
+                        )
+                    )
+                }
+            }
+        }
+        .background(.green)
+        .onAppear {
+            print("현재 genres 개수: \(genres.count)") // 디버깅용
+        }
+    }
+    
+}
