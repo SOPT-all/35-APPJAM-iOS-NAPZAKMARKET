dba766907dc05bb52d4715c5b3df6252650d7246
JoHyerin <gpfls468@gmail.com>
2025-01-14 16:20:36 +0900

    [Feat] #15 - 검색 버튼 추가

--- Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Search/View/SearchView.swift
+++ Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Search/View/SearchView.swift
@@ -41,6 +41,26 @@ extension SearchView {
     
     //MARK: - UI Properties
     
+    private var searchButton: some View {
+        Button {
+            print("searchButtonTapped")
+        } label: {
+            HStack {
+                Text("어떤 아이템을 찾고 계신가요?")
+                    .font(.napzakFont(.body5SemiBold14))
+                    .applyNapzakTextStyle(napzakFontStyle: .body5SemiBold14)
+                    .foregroundStyle(Color.napzakGrayScale(.gray400))
+                Spacer()
+                Image(.iconSearch)
+            }
+            .padding(.horizontal, 20)
+        }
+        .frame(height: 44)
+        .background(Color.napzakGrayScale(.gray100))
+        .cornerRadius(12)
+        .padding(.horizontal, 20)
+    }
+    
     private var productScrollView: some View {
         ScrollView(showsIndicators: false) {
             LazyVGrid(columns: columns, spacing: 20) {
