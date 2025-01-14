6ededebcb69f10fcf8baefb5a667479804a15530
lalaurel <ajinpark0518@naver.com>
2025-01-14 16:37:50 +0900

    [Fix] - #6 모델에 탭 아이템 추가

--- Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Tabbar/Model/TabBarModel.swift
+++ Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Tabbar/Model/TabBarModel.swift
@@ -14,3 +14,15 @@ struct TabItem: Identifiable {
     let selectedIcon: String
     let view: AnyView?
 }
+
+extension TabItem {
+    static func getDefaultTabs() -> [TabItem] {
+        return [
+            TabItem(title: "홈", defaultIcon: "ic_home", selectedIcon: "ic_home_select", view: AnyView(PreView())),
+            TabItem(title: "탐색", defaultIcon: "ic_look", selectedIcon: "ic_look_select", view: AnyView(PreView())),
+            TabItem(title: "등록", defaultIcon: "ic_register", selectedIcon: "ic_register_select", view: AnyView(EmptyView())),
+            TabItem(title: "채팅", defaultIcon: "ic_chat", selectedIcon: "ic_chat_select", view: AnyView(PreView())),
+            TabItem(title: "마이", defaultIcon: "ic_my", selectedIcon: "ic_my_select", view: AnyView(PreView()))
+        ]
+    }
+}
