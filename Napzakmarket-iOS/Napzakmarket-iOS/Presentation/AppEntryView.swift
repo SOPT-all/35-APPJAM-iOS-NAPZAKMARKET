b29cdef32eabfc210f5e910812d11d54b0387bc5
joho2022 <josama1997@naver.com>
2025-01-14 23:19:47 +0900

    [Feat] #9 - OnboardingView 구현

---
+++ Napzakmarket-iOS/Napzakmarket-iOS/Presentation/AppEntryView.swift
@@ -0,0 +1,26 @@
+//
+//  AppEntryView.swift
+//  Napzakmarket-iOS
+//
+//  Created by 조호근 on 1/14/25.
+//
+
+import SwiftUI
+
+struct AppEntryView: View {
+    
+    @State private var isOnboardingComplete: Bool = false
+    
+    var body: some View {
+        if isOnboardingComplete {
+            HomeView()
+        } else {
+            OnboardingView(isOnboardingComplete: $isOnboardingComplete)
+        }
+    }
+    
+}
+
+#Preview {
+    AppEntryView()
+}
--- Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Onboarding/View/OnboardingView.swift
+++ Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Onboarding/View/OnboardingView.swift
@@ -2,6 +2,113 @@
 //  OnboardingView.swift
 //  Napzakmarket-iOS
 //
-//  Created by 조혜린 on 1/6/25.
+//  Created by 조호근 on 1/14/25.
 //
 
+import SwiftUI
+
+struct OnboardingView: View {
+    @StateObject private var genreModel = GenreModel()
+    @Binding var isOnboardingComplete: Bool
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            TitleView()
+            
+            SearchBar(
+                placeholder: "원하는 장르를 직접 입력해 검색해보세요.",
+                text: $genreModel.searchText
+            )
+            .onChange(of: genreModel.searchText) { _ in
+                Task {
+                    try? await genreModel.fetchGenresFiltered()
+                }
+            }
+            
+            ChipsContainerView(
+                selectedGenres: .init(
+                    get: { genreModel.selectedGenres.map { $0.name } },
+                    set: { newNames in
+                        genreModel.removeSelection(newNames )
+                    }
+                )
+            )
+            
+            GenreGridView(
+                genres: $genreModel.genres,
+                selectedGenres: $genreModel.selectedGenres
+            )
+            
+            FinalActionsView(
+                isOnboardingComplete: $isOnboardingComplete,
+                selectedGenres: $genreModel.selectedGenres
+            )
+        }
+        .padding(.horizontal, 20)
+        .task {
+            try? await genreModel.fetchGenres()
+        }
+        
+    }
+}
+
+extension OnboardingView {
+    
+    private struct TitleView: View {
+        
+        var body: some View {
+            VStack(alignment: .leading, spacing: 12) {
+                Text("좋아하는 장르를 골라보세요!\n취향에 딱 맞는 상품을 찾아드릴게요")
+                    .font(.napzakFont(.title1Bold22))
+                    .applyNapzakTextStyle(napzakFontStyle: .title1Bold22)
+                    .foregroundStyle(Color.napzakGrayScale(.gray900))
+                    .lineLimit(2)
+                
+                Text("최대 4개 선택")
+                    .font(.napzakFont(.body5SemiBold14))
+                    .applyNapzakTextStyle(napzakFontStyle: .body5SemiBold14)
+                    .foregroundStyle(Color.napzakGrayScale(.gray500))
+            }
+            .padding(.bottom, 32)
+        }
+    }
+    
+    private struct FinalActionsView: View {
+        @Binding var isOnboardingComplete: Bool
+        @Binding var selectedGenres: [Genre]
+        
+        var body: some View {
+            VStack(spacing: 0) {
+                Button {
+                    isOnboardingComplete = true
+                } label: {
+                    Text("선택완료! 시작하기")
+                        .font(.napzakFont(.body1Bold16))
+                        .applyNapzakTextStyle(napzakFontStyle: .body1Bold16)
+                        .foregroundStyle(Color.napzakGrayScale(.white))
+                        .frame(maxWidth: .infinity, minHeight: 52)
+                        .background(selectedGenres.isEmpty ? Color.napzakGrayScale(.gray400) : Color.napzakPuple(.purple30) )
+                        .cornerRadius(12)
+                }
+                .disabled(selectedGenres.isEmpty)
+                
+                Button {
+                    isOnboardingComplete = true
+                } label: {
+                    Text("선택완료! 시작하기")
+                        .font(.napzakFont(.body2SemiBold16))
+                        .applyNapzakTextStyle(napzakFontStyle: .body2SemiBold16)
+                        .foregroundStyle(Color.napzakGrayScale(.gray500))
+                        .frame(maxWidth: .infinity, minHeight: 52)
+                }
+                Spacer()
+            }
+            .frame(height: 140)
+        }
+    }
+     
+}
+
+#Preview {
+    OnboardingView(isOnboardingComplete: .constant(false))
+}
