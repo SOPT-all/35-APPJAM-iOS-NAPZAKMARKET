1a8614b1f631bc9fbb85a6d9576f8b2d63398733
lalaurel <ajinpark0518@naver.com>
2025-01-15 21:58:23 +0900

    [Style] #24 - 간격 수정

--- Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Tabbar/View/ReadyView.swift
+++ Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Tabbar/View/ReadyView.swift
@@ -32,15 +32,18 @@ struct ReadyView: View {
                     .frame(width: 142, height: 148)
                     .padding(.leading, -21)
                     .padding(.bottom, 20)
+                    .offset(y: -50)
                 
                 VStack(spacing: 6) {
                     Text("준비중이에요")
                         .font(.napzakFont(.title3SemiBold20))
                         .foregroundStyle(Color.napzakGrayScale(.gray800))
+                        .offset(y: -60)
 
                     Text("조금만 기다려주세요!")
                         .font(.napzakFont(.body6Medium14))
                         .foregroundStyle(Color.napzakGrayScale(.gray600))
+                        .offset(y: -60)
                 }
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
