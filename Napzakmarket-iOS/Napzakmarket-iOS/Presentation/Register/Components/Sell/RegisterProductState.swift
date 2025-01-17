a9f62d0d9e1af591956453cfde2dc01e8524bb28
OneTen19 <khy000426@naver.com>
2025-01-18 00:46:24 +0900

    [Chore] - #31 RegisterProductState body 경량화

--- Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Register/Components/Sell/RegisterProductState.swift
+++ Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Register/Components/Sell/RegisterProductState.swift
@@ -15,40 +15,51 @@ struct RegisterProductState: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text("상품 상태")
-                .font(.napzakFont(.body2SemiBold16))
-                .applyNapzakTextStyle(napzakFontStyle: .body2SemiBold16)
-                .foregroundStyle(Color.napzakGrayScale(.gray900))
-                .padding(.bottom, 12)
-            
-            LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 8) {
-                ForEach(options, id: \.self) { option in
-                    Button {
-                        productState = option
-                    } label: {
-                        Text(option)
-                            .font(.napzakFont(.body6Medium14))
-                            .applyNapzakTextStyle(napzakFontStyle: .body6Medium14)
-                            .frame(maxWidth: .infinity, minHeight: 40)
-                            .foregroundColor(
-                                productState == option ? .white : Color
-                                    .napzakGrayScale(.gray900)
-                            )
-                            .background(productState == option ? Color
-                                .napzakGrayScale(.gray900) : .white)
-                            .clipShape(RoundedRectangle(cornerRadius: 12))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 12)
-                                    .stroke(
-                                        Color.napzakGrayScale(.gray200),
-                                        lineWidth: 1
-                                    )
-                            )
-                        
-                    }
-                }
-            }
-            
+            titleSection
+            optionSection
         }
     }
+}
+
+// MARK: - Subviews
+
+extension RegisterProductState {
+    
+    private var titleSection: some View {
+        Text("상품 상태")
+            .font(.napzakFont(.body2SemiBold16))
+            .applyNapzakTextStyle(napzakFontStyle: .body2SemiBold16)
+            .foregroundStyle(Color.napzakGrayScale(.gray900))
+            .padding(.bottom, 12)
+    }
+    
+    private var optionSection: some View {
+        LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 8) {
+            ForEach(options, id: \.self) { option in
+                Button {
+                    productState = option
+                } label: {
+                    Text(option)
+                        .font(.napzakFont(.body6Medium14))
+                        .applyNapzakTextStyle(napzakFontStyle: .body6Medium14)
+                        .frame(maxWidth: .infinity, minHeight: 40)
+                        .foregroundColor(
+                            productState == option ? .white : Color
+                                .napzakGrayScale(.gray900)
+                        )
+                        .background(productState == option ? Color
+                            .napzakGrayScale(.gray900) : .white)
+                        .clipShape(RoundedRectangle(cornerRadius: 12))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 12)
+                                .stroke(
+                                    Color.napzakGrayScale(.gray200),
+                                    lineWidth: 1
+                                )
+                        )
+                }
+            }
+        }
+    }
+    
 }
