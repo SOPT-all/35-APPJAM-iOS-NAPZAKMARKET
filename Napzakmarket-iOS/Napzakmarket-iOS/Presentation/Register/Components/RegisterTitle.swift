63e728d33f0f904f698c492ff8d52fe965b222f9
OneTen19 <khy000426@naver.com>
2025-01-18 00:52:13 +0900

    [Chore] - #31 RegisterTitle body 경량화

--- Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Register/Components/RegisterTitle.swift
+++ Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Register/Components/RegisterTitle.swift
@@ -12,38 +12,50 @@ struct RegisterTitle: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            Text("제목")
-                .font(.napzakFont(.body2SemiBold16))
-                .applyNapzakTextStyle(napzakFontStyle: .body2SemiBold16)
-                .foregroundStyle(Color.napzakGrayScale(.gray900))
-                .padding(.bottom, 12)
-            
-            TextField("제목을 적어주세요.", text: $title)
-                .maxLength(50, text: $title)
-                .font(.napzakFont(.body6Medium14))
-                .applyNapzakTextStyle(napzakFontStyle: .body6Medium14)
-                .foregroundStyle(Color.napzakGrayScale(.gray900))
-                .padding(.horizontal, 14)
-                .padding(.vertical, 11)
-                .frame(height: 42)
-                .background {
-                    RoundedRectangle(cornerRadius: 12)
-                        .stroke(Color.napzakGrayScale(.gray200), lineWidth: 1)
-                }
-            
-            HStack(spacing: 0) {
-                Spacer()
-                Text(title.count.description)
-                    .foregroundStyle(title.count == 0 ? Color.napzakGrayScale(.gray400) : Color.napzakGrayScale(.gray900))
-                
-                Text("/50")
-                    .foregroundStyle(Color.napzakGrayScale(.gray400))
-            }
-            .font(.napzakFont(.caption3Medium12))
-            .applyNapzakTextStyle(napzakFontStyle: .caption3Medium12)
-            .padding(.top, 4)
-            
+            titleSection
+            inputTitleSection
+            countTitleSection
         }
-        
     }
+}
+
+extension RegisterTitle {
+    
+    private var titleSection: some View {
+        Text("제목")
+            .font(.napzakFont(.body2SemiBold16))
+            .applyNapzakTextStyle(napzakFontStyle: .body2SemiBold16)
+            .foregroundStyle(Color.napzakGrayScale(.gray900))
+            .padding(.bottom, 12)
+    }
+    
+    private var inputTitleSection: some View {
+        TextField("제목을 적어주세요.", text: $title)
+            .maxLength(50, text: $title)
+            .font(.napzakFont(.body6Medium14))
+            .applyNapzakTextStyle(napzakFontStyle: .body6Medium14)
+            .foregroundStyle(Color.napzakGrayScale(.gray900))
+            .padding(.horizontal, 14)
+            .padding(.vertical, 11)
+            .frame(height: 42)
+            .background {
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color.napzakGrayScale(.gray200), lineWidth: 1)
+            }
+    }
+    
+    private var countTitleSection: some View {
+        HStack(spacing: 0) {
+            Spacer()
+            Text(title.count.description)
+                .foregroundStyle(title.count == 0 ? Color.napzakGrayScale(.gray400) : Color.napzakGrayScale(.gray900))
+            
+            Text("/50")
+                .foregroundStyle(Color.napzakGrayScale(.gray400))
+        }
+        .font(.napzakFont(.caption3Medium12))
+        .applyNapzakTextStyle(napzakFontStyle: .caption3Medium12)
+        .padding(.top, 4)
+    }
+    
 }
