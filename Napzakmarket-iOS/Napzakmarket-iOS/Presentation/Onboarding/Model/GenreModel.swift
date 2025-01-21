17f6aa301ab7c86940578479ca9c0eab8c5903db
joho2022 <josama1997@naver.com>
2025-01-22 03:36:39 +0900

    [Refactor] #54 - GenreModel 네트워크 통신 적용 및 데이터 모델 변경

--- Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Onboarding/Model/GenreModel.swift
+++ Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Onboarding/Model/GenreModel.swift
@@ -12,33 +12,47 @@ final class GenreModel: ObservableObject {
     
     // MARK: - Properties
     
-    @Published var genres: [Genre] = []
-    @Published var selectedGenres: [Genre] = []
+    @Published var genres: [PreferGenre] = []
+    @Published var selectedGenres: [PreferGenre] = []
     @Published var searchText: String = ""
-    
-    private let service: MockGenreService
-    
-    init(service: MockGenreService = .shared) {
-        self.service = service
-    }
     
     // MARK: - Func
     
-    func fetchGenres() async throws {
-        do {
-            genres = try await service.fetchGenres()
-            print("가져온 장르 데이터: \(genres)")
-        } catch {
-            print("Error fetching genres: \(error.localizedDescription)")
+    func fetchGenres() async {
+        NetworkService.shared.genreService.getAllPreferGenre { [weak self] result in
+            switch result {
+            case .success(let dto):
+                if let data = dto?.data {
+                    self?.genres = data.genreList
+                }
+                
+            default:
+                break
+            }
         }
     }
     
-    func fetchGenresFiltered() async throws {
-        self.genres = try await service.fetchGenresFiltered(by: searchText)
+    func fetchGenresFiltered() async {
+        guard !searchText.isEmpty else {
+            await fetchGenres()
+            return
+        }
+        
+        NetworkService.shared.genreService.getSearchPreferGenre(searchWord: searchText) { [weak self] result in
+            switch result {
+            case .success(let dto):
+                if let data = dto?.data {
+                    self?.genres = data.genreList
+                }
+                
+            default:
+                break
+            }
+        }
     }
     
-    func removeSelection(_ newNames: [String] ) {
-        selectedGenres.removeAll { !newNames.contains($0.name) }
+    func removeSelection(_ selectedIDs: [Int]) {
+        selectedGenres.removeAll { !selectedIDs.contains($0.id) }
     }
     
 }
