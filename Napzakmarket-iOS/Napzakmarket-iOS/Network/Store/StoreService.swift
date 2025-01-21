ce635dd29ad61e39a1f26ac70a65d83f123e2cd6
lalaurel <ajinpark0518@naver.com>
2025-01-22 04:51:50 +0900

    [Fix] #57 - mypage -> store로 수정

--- Napzakmarket-iOS/Napzakmarket-iOS/Network/Store/StoreService.swift
+++ Napzakmarket-iOS/Napzakmarket-iOS/Network/Store/StoreService.swift
@@ -8,15 +8,15 @@
 import Foundation
 import Moya
 
-protocol MyPageServiceProtocol {
-    func fetchMyPageInfo(completion: @escaping (NetworkResult<MyPageInfoResponseDTO>) -> ())
+protocol StoreServiceProtocol {
+    func getStoreInfo(completion: @escaping (NetworkResult<MyPageInfoResponseDTO>) -> ())
 }
 
-final class MyPageService: BaseService, MyPageServiceProtocol {
+final class StoreService: BaseService, StoreServiceProtocol {
     let provider = MoyaProvider<MyPageAPI>()
     
-    func fetchMyPageInfo(completion: @escaping (NetworkResult<MyPageInfoResponseDTO>) -> ()) {
-        provider.request(.fetchMyPageInfo) { result in
+    func getStoreInfo(completion: @escaping (NetworkResult<MyPageInfoResponseDTO>) -> ()) {
+        provider.request(.getStoreInfo) { result in
             switch result {
             case .success(let response):
                 let networkResult: NetworkResult<MyPageInfoResponseDTO> = self.fetchNetworkResult(
