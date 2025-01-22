2359f949f696565007a4a8f5e76b15a5aaea1f92
lalaurel <ajinpark0518@naver.com>
2025-01-23 06:07:27 +0900

    [Fix] #73 - 모델 삭제

--- Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Market/Model/MarketModel.swift
+++ Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Market/Model/MarketModel.swift
@@ -4,23 +4,3 @@
 //
 //  Created by 어진 on 1/15/25.
 //
-
-import Foundation
-
-struct Tag: Identifiable {
-    let id: String
-    let name: String
-}
-
-//MockData
-struct MarketMockData {
-    static let tags: [Tag] = [
-        Tag(id: UUID().uuidString, name: "최강아요"),
-        Tag(id: UUID().uuidString, name: "납작어진"),
-        Tag(id: UUID().uuidString, name: "납작혜린"),
-        Tag(id: UUID().uuidString, name: "납작호근"),
-        Tag(id: UUID().uuidString, name: "납작한열")
-    ]
-}
-
-
