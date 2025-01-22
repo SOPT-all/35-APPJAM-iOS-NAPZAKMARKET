f99c4c4e1b99da318289e58cceae568a40df0cb4
JoHyerin <gpfls468@gmail.com>
2025-01-23 01:38:36 +0900

    [Feat] #61 - ProductFetchOption 객체 생성 및 ProductSearchModel 구현

---
+++ Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Search/Model/ProductSearchModel.swift
@@ -0,0 +1,53 @@
+//
+//  ProductSearchModel.swift
+//  Napzakmarket-iOS
+//
+//  Created by 조혜린 on 1/16/25.
+//
+
+import Foundation
+
+struct ProductFetchOption: Equatable {
+    var sortOption: SortOption
+    var genreIDs: [Int]
+    var isOnSale: Bool
+    var isUnopened: Bool
+}
+
+final class ProductSearchModel: ObservableObject {
+
+    //MARK: - Property Wrappers
+
+    @Published var sellProducts: [ProductModel] = []
+    @Published var buyProducts: [ProductModel] = []
+}
+
+extension ProductSearchModel {
+
+    //MARK: - API Request Func
+
+    func getSellProducts(productFetchOption: ProductFetchOption) async {
+        NetworkService.shared.productService.getSellProduct(sortOption: productFetchOption.sortOption.rawValue, genreIDs: productFetchOption.genreIDs, isOnSale: productFetchOption.isOnSale, isUnopened: productFetchOption.isUnopened) { [weak self] result in
+            switch result {
+            case .success(let response):
+                guard let response else { return }
+                self?.sellProducts = response.data.productSellList.map { ProductModel(dto: $0) }
+            default:
+                break
+            }
+        }
+    }
+
+    func getBuyProducts(productFetchOption: ProductFetchOption) async {
+        NetworkService.shared.productService.getBuyProduct(sortOption: productFetchOption.sortOption.rawValue, genreIDs: productFetchOption.genreIDs, isOnSale: productFetchOption.isOnSale) { [weak self] result in
+            switch result {
+            case .success(let response):
+                guard let response else { return }
+                self?.buyProducts = response.data.productBuyList.map { ProductModel(dto: $0) }
+            default:
+                break
+            }
+        }
+    }
+
+}
