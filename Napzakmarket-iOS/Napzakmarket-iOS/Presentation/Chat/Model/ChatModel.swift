11f83c92ccf61ec81a5b3a98b17838cbd44c5139
lalaurel <ajinpark0518@naver.com>
2025-01-24 04:45:15 +0900

    [Fix] #69 - 불필요한 코드 삭제

--- Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Chat/Model/ChatModel.swift
+++ Napzakmarket-iOS/Napzakmarket-iOS/Presentation/Chat/Model/ChatModel.swift
@@ -4,18 +4,3 @@
 //
 //  Created by 어진 on 1/24/25.
 //
-
-import Foundation
-
-@MainActor
-final class ChatViewModel: ObservableObject {
-    @Published var chatInfo: ChatInfoData?
-    
-    private let productService: ProductServiceProtocol
-    private let productId: Int
-    
-    init(productId: Int, productService: ProductServiceProtocol = ProductService()) {
-        self.productId = productId
-        self.productService = productService
-    }
-}
